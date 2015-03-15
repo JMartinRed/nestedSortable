95a75ef8a0fe37f36e9afdee5f5466b3351ddf3b
JMartinRed <almachareno@yahoo.com>
2015-03-15 08:03:11 +0100

    Update jquery.mjs.nestedSortable.js
    collapsedAll will enable us to expand all or collapse all at any time we need.

--- jquery.mjs.nestedSortable.js
+++ jquery.mjs.nestedSortable.js
@@ -658,7 +658,31 @@
 			}
 
 		},
-
+		
+		
+		_setOptions: function( options ) {
+		  	var that = this,
+				opti = that.option();
+		 
+		  	$.each( options, function( key, value ) {
+				that._setOption( key, value );
+				if ( key === "startCollapsed") {
+					console.log(that.opti);
+					that.element
+					.find('.'+opti.collapsedClass+', .'+opti.expandedClass+', .'+opti.leafClass)
+					.removeClass(opti.collapsedClass+' '+opti.expandedClass+' '+opti.leafClass);
+			  		
+					that._create();
+				}
+		  	});
+		 
+		},
+		
+		collapsedAll: function(state) {
+		
+			this.option({startCollapsed: state});
+		},
+		
 		serialize: function(options) {
 
 			var o = $.extend({}, this.options, options),
